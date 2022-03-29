32f5d7b2ac5e19163611fd993d493f6f5573be5e
v4lb3r <3cifhzht@duck.com>
2022-03-29 15:11:23 -0300

    created referall class

---
+++ clean-architecture-school/src/main/java/com/jr/school/Referall.java
@@ -0,0 +1,13 @@
+package com.jr.school;
+
+public class Referall {
+
+    private Student referred;
+    private Student indicator;
+
+    public Referall(Student referred, Student indicator) {
+        this.referred = referred;
+        this.indicator = indicator;
+    }
+
+}
