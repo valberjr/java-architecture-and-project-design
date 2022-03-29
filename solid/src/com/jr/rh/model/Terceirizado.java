c685496a740347dde02a7e5373c1ddcad6d15f9d
v4lb3r <3cifhzht@duck.com>
2022-03-29 10:42:35 -0300

    composicao + Liskov Substitution Principle

--- solid/src/com/jr/rh/model/Terceirizado.java
+++ solid/src/com/jr/rh/model/Terceirizado.java
@@ -2,19 +2,33 @@ package com.jr.rh.model;
 
 import java.math.BigDecimal;
 
-public class Terceirizado extends Funcionario {
+public class Terceirizado {
 
+    private DadosPessoais dadosPessoais;
     private String empresa;
 
     public Terceirizado(String nome, String cpf, Cargo cargo, BigDecimal salario) {
-        super(nome, cpf, cargo, salario);
+        this.dadosPessoais = new DadosPessoais(nome, cpf, cargo, salario);
+        this.empresa = getEmpresa();
     }
 
     public String getEmpresa() {
         return empresa;
     }
 
-    public void setEmpresa(String empresa) {
-        this.empresa = empresa;
+    public String getNome() {
+        return this.dadosPessoais.getNome();
+    }
+
+    public String getCpf() {
+        return this.dadosPessoais.getCpf();
+    }
+
+    public Cargo getCargo() {
+        return this.dadosPessoais.getCargo();
+    }
+
+    public BigDecimal getSalario() {
+        return this.dadosPessoais.getSalario();
     }
 }
