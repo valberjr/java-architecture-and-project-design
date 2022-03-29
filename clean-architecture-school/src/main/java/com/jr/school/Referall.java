959dd4817b69a794f41e4f44e1a2595520c8d6f4
v4lb3r <3cifhzht@duck.com>
2022-03-29 15:14:05 -0300

    added referall date + getters

--- clean-architecture-school/src/main/java/com/jr/school/Referall.java
+++ clean-architecture-school/src/main/java/com/jr/school/Referall.java
@@ -1,13 +1,28 @@
 package com.jr.school;
 
+import java.time.LocalDateTime;
+
 public class Referall {
 
     private Student referred;
     private Student indicator;
+    private LocalDateTime referallDate;
 
     public Referall(Student referred, Student indicator) {
         this.referred = referred;
         this.indicator = indicator;
+        this.referallDate = LocalDateTime.now();
     }
 
+    public Student getReferred() {
+        return referred;
+    }
+
+    public Student getIndicator() {
+        return indicator;
+    }
+
+    public LocalDateTime getReferallDate() {
+        return referallDate;
+    }
 }
