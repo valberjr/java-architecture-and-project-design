73f2a20d08126fe5ca17af90610780f222770532
v4lb3r <3cifhzht@duck.com>
2022-03-29 15:28:18 -0300

    factories

---
+++ clean-architecture-school/src/main/java/com/jr/school/StudentFactory.java
@@ -0,0 +1,22 @@
+package com.jr.school;
+
+// it's not an application of factory method
+public class StudentFactory {
+
+    private Student student;
+
+    public StudentFactory withNameCPFAndEmail(String name, String cpf, String email) {
+        this.student = new Student(new CPF(cpf), name, new Email(email));
+        return this;
+    }
+
+    public StudentFactory withPhone(String areaCode, String number) {
+        this.student.addPhone(areaCode, number);
+        return this;
+    }
+
+    public Student create() {
+        return this.student;
+    }
+
+}
