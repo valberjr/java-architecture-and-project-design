8a2850481124b1e05402e02d03e7016ebac71210
v4lb3r <3cifhzht@duck.com>
2022-03-29 15:01:53 -0300

    adding phones + tests

--- clean-architecture-school/src/main/java/com/jr/school/Student.java
+++ clean-architecture-school/src/main/java/com/jr/school/Student.java
@@ -1,5 +1,8 @@
 package com.jr.school;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
 
     private CPF cpf;
@@ -7,4 +10,25 @@ public class Student {
 
     private Email email;
 
+    private List<Phone> phones = new ArrayList<>();
+
+    public void addPhone(String ddd, String number) {
+        this.phones.add(new Phone(ddd, number));
+    }
+
+    public CPF getCpf() {
+        return cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public List<Phone> getPhones() {
+        return phones;
+    }
 }
