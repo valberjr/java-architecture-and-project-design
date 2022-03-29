e4c258ca9cc6826b4201a3a30108bca0f017b16d
v4lb3r <3cifhzht@duck.com>
2022-03-29 14:22:09 -0300

    value objects

--- clean-architecture-school/src/main/java/com/jr/school/Email.java
+++ clean-architecture-school/src/main/java/com/jr/school/Email.java
@@ -1,4 +1,16 @@
 package com.jr.school;
 
+// value object
 public class Email {
+
+    private String address;
+
+    public Email(String address) {
+        if (address == null || !address.matches("^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\\\.[a-zA-Z]{2,}$")) {
+            throw new IllegalArgumentException("Invalid Email");
+        }
+
+        this.address = address;
+    }
+
 }
