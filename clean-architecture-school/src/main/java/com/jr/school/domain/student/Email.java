225b3e092971c6c7739959b6f2293668bb3e7cfd
v4lb3r <3cifhzht@duck.com>
2022-03-30 09:24:04 -0300

    fix email regex

--- clean-architecture-school/src/main/java/com/jr/school/domain/student/Email.java
+++ clean-architecture-school/src/main/java/com/jr/school/domain/student/Email.java
@@ -6,7 +6,7 @@ public class Email {
     private String address;
 
     public Email(String address) {
-        if (address == null || !address.matches("^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\\\.[a-zA-Z]{2,}$")) {
+        if (address == null || !address.matches("^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
             throw new IllegalArgumentException("Invalid Email");
         }
 
